7064b75d5eaf54c5d9c1a096578ba06bb4597b31
mazzon <filipe.mazzon.ribeiro@gmail.com>
2023-06-15 16:44:33 -0300

    add url server to env variable

--- src/infraestructure/config/server.config.ts
+++ src/infraestructure/config/server.config.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const SERVER_URL = 'http://localhost:4000';
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL ||  'http://localhost:4000';
